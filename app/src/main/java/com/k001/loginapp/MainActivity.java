786eb230a3693e33502ca1b60669baafdd8d9ef8
Jonas Khanh <nguyenkhanhcv@gmail.com>
2019-07-19 20:30:38 +0700

    Add main file

---
+++ app/src/main/java/com/k001/loginapp/MainActivity.java
@@ -0,0 +1,21 @@
+package com.k001.loginapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void signUp(View view) {
+        Intent intent = new Intent(this, SignUp.class);
+        startActivity(intent);
+    }
+}
